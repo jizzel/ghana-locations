e0f859d6b767e110c3e900bbb87ccc20a9425106
Joseph Afriyie Attakorah <juxluvjoe@gmail.com>
2026-02-06 09:28:19 +0000

    feat: add more cities for all regions - closes #5 #6 #7 #8 #9 #10 #11 #12 #13 #14 #15 #16 #17 #18 #19 #20

--- src/data/cities.ts
+++ src/data/cities.ts
@@ -2,51 +2,51 @@ import { RegionName } from "./regions";
 
 export const citiesByRegion: Record<RegionName, string[]> = {
   [RegionName.GreaterAccra]: [
-    "Accra", "Tema", "Madina", "Ashiaman", "Adenta", "Teshie", "Nungua", "Abokobi", "Dodowa", "Prampram", "Ada Foah", "Kpone", "Osu", "Amasaman"
+    "Accra", "Tema", "Madina", "Ashiaman", "Adenta", "Teshie", "Nungua", "Abokobi", "Dodowa", "Prampram", "Ada Foah", "Kpone", "Osu", "Amasaman", "Latebiokorshie", "Ablekuma North", "Dansoman", "Kokomlemle", "Nima", "Accra New Town", "Dzorwulu", "Ngleshie Amanfrom", "Sowutuom", "Ofankor", "La", "Abeka", "Tema Community 2", "Weija", "Sege"
   ],
   [RegionName.Ashanti]: [
-    "Kumasi", "Obuasi", "Mampong", "Konongo", "Ejura", "Bekwai", "Agogo", "Juaso", "Tepa", "Effiduase", "Ejisu", "Offinso"
+    "Kumasi", "Obuasi", "Mampong", "Konongo-Odumase", "Ejura", "Bekwai", "Agogo", "Juaso", "Tepa", "Effiduase", "Ejisu", "Offinso", "Asokore", "Asokwa", "Nkawie", "Juaben", "Mamponteng", "Kwadaso", "Oforikrom", "Old Tafo", "Suame", "Fomena", "New Edubiase", "Boaman", "Kodie", "Mankranso", "Adugyama", "Akrofuom", "Jacobu", "Edubia", "Manso Nkwanta", "Foase", "Nyinahin", "Barekese", "Asiwa", "Kuntenase", "Tutuka", "Akomadan", "Drobonso", "Nsuta", "Kumawu", "Agona"
   ],
   [RegionName.Western]: [
-    "Sekondi-Takoradi", "Tarkwa", "Axim", "Elubo", "Agona Nkwanta", "Dixcove", "Shama", "Prestea", "Half Assini"
+    "Sekondi-Takoradi", "Tarkwa", "Axim", "Elubo", "Agona Nkwanta", "Dixcove", "Shama", "Prestea", "Half Assini", "Wassa Akropong", "Asankragu", "Kwesimintsim", "Nkroful", "Mpohor", "Bogoso", "Daboase"
   ],
   [RegionName.WesternNorth]: [
-    "Sefwi Wiawso", "Bibiani", "Enchi", "Dadieso", "Juaboso", "Bodi", "Akontombra"
+    "Sefwi Wiawso", "Bibiani", "Enchi", "Dadieso", "Juaboso", "Bodi", "Akontombra", "Adabokrom", "Essam"
   ],
   [RegionName.Central]: [
-    "Cape Coast", "Winneba", "Kasoa", "Mankessim", "Saltpond", "Elmina", "Apam", "Agona Swedru", "Dunkwa-on-Offin", "Assin Fosu"
+    "Cape Coast", "Winneba", "Kasoa", "Mankessim", "Saltpond", "Elmina", "Apam", "Agona Swedru", "Dunkwa-on-Offin", "Assin Fosu", "Abura Dunkwa", "Nsaba", "Ajumako", "Breman Asikuma", "Assin Bereku", "Kyekyewere", "Awutu Beraku", "Essarkyir", "Afransi", "Potsin", "Hemang", "Twifo Praso", "Diaso"
   ],
   [RegionName.Eastern]: [
-    "Koforidua", "Nkawkaw", "Nsawam", "Akim Oda", "Asamankese", "Mpraeso", "Somanya", "Kibi", "Begoro", "Aburi", "Suhum"
+    "Koforidua", "Nkawkaw", "Nsawam", "Akim Oda", "Asamankese", "Mpraeso", "Somanya", "Kibi", "Begoro", "Aburi", "Suhum", "Kukurantumi", "Akropong", "Kade", "Odumase Krobo", "Achiase", "Ofoase", "Atimpoku", "Anyinam", "Kwabeng", "Coaltar", "New Abirem", "Akim Swedru", "Akwatia", "Osino", "Donkorkrom", "Abetifi", "Adukrom", "Asesewa", "Adeiso"
   ],
   [RegionName.Volta]: [
-    "Ho", "Hohoe", "Keta", "Aflao", "Anloga", "Akatsi", "Sogakope", "Kpando", "Dzodze"
+    "Ho", "Hohoe", "Keta", "Aflao", "Anloga", "Akatsi", "Sogakope", "Kpando", "Dzodze", "Adaklu Waya", "Ve Golokwati", "Agortime Kpetoe", "Ave-Dakpa", "Adidome", "Dzolokpuita", "Denu", "Anfoega", "Battor Dugame", "Kpeve"
   ],
   [RegionName.Oti]: [
-    "Dambai", "Jasikan", "Kadjebi", "Nkwanta", "Chinderi", "Krachi", "Kpetoe"
+    "Dambai", "Jasikan", "Kadjebi", "Nkwanta", "Chinderi", "Kete Krachi", "Kpassa", "Likpe Mate", "Nkonya Ahenkro"
   ],
   [RegionName.Northern]: [
-    "Tamale", "Yendi", "Savelugu", "Bimbilla", "Gushiegu", "Kumbungu", "Wulensi"
+    "Tamale", "Yendi", "Savelugu", "Bimbilla", "Gushegu", "Kumbungu", "Wulensi", "Sagnarigu", "Karaga", "Kpandai", "Sang", "Nanton", "Saboba", "Tatale", "Tolon", "Zabzugu"
   ],
   [RegionName.Savannah]: [
-    "Damongo", "Buipe", "Salaga", "Sawla", "Bole", "Daboya"
+    "Damongo", "Buipe", "Salaga", "Sawla", "Bole", "Daboya", "Kpalbe"
   ],
   [RegionName.NorthEast]: [
-    "Nalerigu", "Gambaga", "Walewale", "Bunkpurugu", "Chereponi", "Yagaba"
+    "Nalerigu", "Gambaga", "Walewale", "Bunkpurugu", "Chereponi", "Yagaba", "Yunyoo"
   ],
   [RegionName.UpperEast]: [
-    "Bolgatanga", "Bawku", "Navrongo", "Paga", "Tongo", "Zuarungu", "Sandema"
+    "Bolgatanga", "Bawku", "Navrongo", "Paga", "Tongo", "Zuarungu", "Sandema", "Zebilla", "Binduri", "Bongo", "Fumbisi", "Garu", "Nangodi", "Pusiga", "Tempane"
   ],
   [RegionName.UpperWest]: [
-    "Wa", "Tumu", "Jirapa", "Lambussie", "Lawra", "Nandom", "Gwollu"
+    "Wa", "Tumu", "Jirapa", "Lambussie", "Lawra", "Nandom", "Gwollu", "Issa", "Nadowli", "Funsi", "Wechiau"
   ],
   [RegionName.Bono]: [
-    "Sunyani", "Berekum", "Dormaa Ahenkro", "Sampa", "Wenchi", "Drobo"
+    "Sunyani", "Berekum", "Dormaa Ahenkro", "Sampa", "Wenchi", "Drobo", "Nsawkaw", "Odumase", "Wamfie", "Banda Ahenkro"
   ],
   [RegionName.BonoEast]: [
-    "Techiman", "Kintampo", "Nkoranza", "Yeji", "Prang", "Atebubu"
+    "Techiman", "Kintampo", "Nkoranza", "Yeji", "Prang", "Atebubu", "Kwame Danso", "Jema", "Busunya", "Tuobodom", "Kajaji"
   ],
   [RegionName.Ahafo]: [
-    "Goaso", "Bechem", "Duayaw Nkwanta", "Kenyasi", "Hwidiem"
+    "Goaso", "Bechem", "Duayaw Nkwanta", "Kenyasi", "Hwidiem", "Kukuom"
   ],
 };
